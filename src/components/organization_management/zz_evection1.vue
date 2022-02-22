70d9bb41b43e3d1f59f9ad74fda023b6ff069e73
彭紫炎 <1269353044@qq.com>
2022-02-22 14:26:52 +0800

    Changes

--- src/components/organization_management/zz_evection1.vue
+++ src/components/organization_management/zz_evection1.vue
@@ -2,7 +2,7 @@
   <div class="w">
     <div class="head">
       <el-button type="primary" style="margin-left: 16px" @click="dialogVisible=true"
-      >新增
+      >新增1
       </el-button>
       <el-dialog
           v-model="dialogVisible"
@@ -47,27 +47,55 @@
     </div>
 
     <div class="y">
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column type="expand">
-          <template #default="props">
-            <p>部门负责人: {{ props.row.state }}</p>
-            <p>City: {{ props.row.city }}</p>
-            <p>Address: {{ props.row.address }}</p>
-            <p>Zip: {{ props.row.zip }}</p>
-          </template>
-        </el-table-column>
-        <el-table-column label="ID" prop="deptId" />
-        <el-table-column label="部门" prop="deptName" />
-		<el-table-column label="部门负责人" prop="staffName" />
-        <el-table-column label="状态"   >
+      <el-table :data="tableData"
+                v-if="refreshTable"
+                row-key="deptId"
+                :default-expand-all="expands"
+                @expand-change="expands=true"
+                stripe style="width: 100% ;"
+                :header-cell-style="{textAlign: 'center',background:'#f8f8f9',color:'#6C6C6C'}"
+                :cell-style="{textAlign: 'center'}"
+      >
+        <!--        <el-table-column type="expand">-->
+        <!--          <template #default="scope">-->
+
+        <!--            <span style="margin-left: 50px">部门负责人：{{ scope.row.staffName }}</span><br>-->
+        <!--            <span style="margin-left: 50px">部门人数：{{ scope.row.empNum }}</span>-->
+
+        <!--          </template>-->
+        <!--        </el-table-column>-->
+
+        <el-table-column prop="deptName" label="部门名称"/>
+        <el-table-column prop="staffName" label="部门负责人"/>
+        <el-table-column prop="createdTime" label="创建时间"/>
+        <el-table-column prop="updatedTime" label="修改时间"/>
+        <el-table-column prop="deptState" label="状态">
+
           <template #default="scope">
-            <span v-if="scope.row.deptState===1">禁用</span>
-            <span v-if="scope.row.deptState===0">启用</span>
+
+            <el-button v-if="scope.row.deptState==0" size="mini" round type="primary" plain>启用</el-button>
+
+            <el-button v-if="scope.row.deptState==1" size="mini" round type="danger" plain>禁用</el-button>
           </template>
         </el-table-column>
-<!--      <template #default>
-        <span v-if=""></span>
-      </template>-->
+
+        <el-table-column prop="createdTime" label="详情">
+
+          <el-popover
+              placement="bottom"
+              title="部门信息"
+              :width="100"
+              trigger="click"
+          >
+            <span>部门负责人:微信</span><br><br>
+            <span>部门总人数:18</span>
+            <template #reference>
+              <span style="color:#409eff">查看详情</span>
+            </template>
+
+          </el-popover>
+
+        </el-table-column>
 
         <el-table-column prop="operate" label="操作" >
           <template #default="scope">
@@ -77,22 +105,21 @@
                 <el-button type="text" size="small" style="color:darkorange" >删除 </el-button>
               </template>
             </el-popconfirm>
-            <el-button type="text" size="small" style="color:darkorange" @click="dialogVisible11=true, deptStaff={deptId:tableData[scope.$index].deptId,deptName:tableData[scope.$index].deptName,staffId: tableData[scope.$index].staffId,deptState:tableData[scope.$index].deptState }">修改
+            <el-button type="text" size="small" style="color:darkorange" @click="dialogVisible11=true,this.dept11={},yy1(scope.row)">修改
             </el-button>
 
           </template>
         </el-table-column>
       </el-table>
-
     </div>
     <el-drawer v-model="dialogVisible11" title="I am the title" :with-header="false">
-      <el-form ref="form" :model="deptStaff" label-width="120px">
+      <el-form ref="form" :model="dept12" label-width="120px">
         <el-form-item label="部门名称：">
-          <el-input v-model="deptStaff.deptName"></el-input>
+          <el-input v-model="dept12.deptName"></el-input>
         </el-form-item>
         <el-form-item label="部门负责人：">
 
-          <el-select v-model="deptStaff.staffId" placeholder="请选择部门负责人">
+          <el-select v-model="dept12.staffId" placeholder="请选择部门负责人1">
             <el-option
                 v-for="item in ygname"
                 :key="item"
@@ -105,16 +132,16 @@
           </el-select>
         </el-form-item>
         <el-form-item label="部门状态：  ">
-        <el-radio-group v-model="deptStaff.deptState">
-          <el-radio :label="0">启用</el-radio>
+        <el-radio-group v-model="dept12.deptState">
+          <el-radio :label="0">启用1</el-radio>
           <el-radio :label="1">禁用</el-radio>
         </el-radio-group>
-          aa{{this.deptStaff.staffId}}
+          aa{{this.dept12.staffId}}
         </el-form-item>
         <span class="dialog-footer">
-        <el-button @click="dialogVisible11 = false">取消</el-button>
+        <el-button @click="dialogVisible11 = false">取消1</el-button>
         <el-button type="primary" @click="dialogVisible11 = false,updatedept()"
-        >确定</el-button
+        >确定1</el-button
         >
       </span>
       </el-form>
@@ -160,6 +187,8 @@ export default {
         birthday:'',
         loc:''
       },
+      refreshTable:true,
+      expands:false,
       deptStaff:{
         deptId:"",
         deptName:"",
@@ -178,6 +207,24 @@ export default {
         pageSize: 3,
         total: 0,
         deptName:'',
+      },
+      dept11:{
+        deptId:'',
+        menuPid:'',
+        menuLeaf:'',
+        staffName:'',
+        deptState:'',
+        deptName:'',
+        staffId:'',
+      },
+      dept12:{
+        deptId:'',
+        menuPid:'',
+        menuLeaf:'',
+
+        deptState:'',
+        deptName:'',
+        staffId:'',
       },
       dialogVisible11:false,
       dialogVisible:false,
@@ -205,12 +252,22 @@ export default {
 
   },
   methods: {
+    yy1(row){
+      this.dept11.deptName=row.deptName
+      this.dept11.staffName=row.staffName
+      this.dept11.staffId=row.staffId
+      this.dept11.deptState=row.deptState
+      this.dept12.deptName=row.deptName
+     this.dept12.deptId=row.deptId
+      this.dept12.staffId=row.staffId
+      this.dept12.deptState=row.deptState
+    },
  //修改部门
-    updatedept(){
+  updatedept(){
       this.axios({
         method:"put",
         url:"http://localhost:8007/provider/updateDept/dept",
-        data:this.deptStaff,
+        data:this.dept12,
         responseType:'json',
         responseEncoding:"utf-8"
       }).then(response=>{
@@ -232,6 +289,7 @@ export default {
        params:this.options,
           })
           .then((response) => {
+            alert(1)
             console.log("查询cao");
             console.log(response.data);
            /* alert(this.pageInfo.currentPage+"this.pageInfo.currentPage")*/
@@ -249,7 +307,7 @@ export default {
           });
     },
 
-    addDept(){
+  /*  addDept(){
       this.axios.post("http://localhost:8007/provider/addDept",this.dept)
           .then(response => {
             this.dept.deptId=null
@@ -260,6 +318,7 @@ export default {
                 message: '添加成功',
                 type: 'success',
               })
+              this.selectEndAuditflow();
             }
           }).catch(function (error) {
         console.log(error);
@@ -267,7 +326,7 @@ export default {
 
 
 
-    },
+    },*/
     deleteUser(id) {
       this.axios({
         url: "http://localhost:8007/provider/deleteUser",
@@ -295,7 +354,7 @@ export default {
             console.log(error);
           });
     },
-      //主查询
+     /* //主查询
     selectEndAuditflow() {
       this.axios({
         method:'post',
@@ -316,6 +375,15 @@ export default {
           });
 
 
+    },*/
+    selectEndAuditflow(){
+      this.axios.get("http://localhost:8007/provider/chadeptt").then((rersponse)=>{
+        this.tableData=rersponse.data.data;
+        console.log(rersponse.data.data[0].children)
+        console.error(rersponse.data.data)
+      }).catch(function (error){
+        console.log(error);
+      })
     },
 
   }, created() {
